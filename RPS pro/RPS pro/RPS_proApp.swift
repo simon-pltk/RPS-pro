47c1219bc2403cf819fe36e0f4ffa4799be45628
simon-pltk <simplot75@gmail.com>
2024-06-20 11:45:17 -0400

    Start

---
+++ RPS pro/RPS pro/RPS_proApp.swift
@@ -0,0 +1,17 @@
+//
+//  RPS_proApp.swift
+//  RPS pro
+//
+//  Created by Simon Plotkin on 6/20/24.
+//
+
+import SwiftUI
+
+@main
+struct RPS_proApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
